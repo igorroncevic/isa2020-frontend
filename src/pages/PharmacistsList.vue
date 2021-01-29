10f31b78540b75a4a233ff0d2c3db5c9a37b1ce6
zukovicfilip <zukovicfilip@gmail.com>
2021-01-29 20:36:11 +0100

    feat.33 - Implemented filtering by mark, pharmacy and name

--- src/pages/PharmacistsList.vue
+++ src/pages/PharmacistsList.vue
@@ -6,16 +6,28 @@
       :columns="columns"
       row-key="id"
       :filter="filter"
+      :filter-method="myFilter"
       :loading="loading"
     >
 
       <template v-slot:top>
         <q-space />
-        <q-input borderless dense debounce="300" color="primary" v-model="filter">
-          <template v-slot:append>
-            <q-icon name="search" />
-          </template>
-        </q-input>
+        <div class="q-pa-sm">
+          <q-btn round color="red" icon="highlight_off" @click="removeFilters" v-show=filtersActive />
+        </div>
+        <div class="q-pa-sm">
+          <q-select v-model="filter.pharmacy" :options="options" label="Pharmacy" style="min-width: 200px"/>
+        </div>
+        <div class="q-pa-sm">
+          <q-input v-model.number="filter.minMark" type="number" label="Minimum average mark" style="min-width: 200px"/>
+        </div>
+        <div class="q-pa-sm">
+          <q-input v-model="filter.query" label="Search names" style="min-width: 200px">
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </div>
       </template>
 
     </q-table>
@@ -29,11 +41,22 @@ export default {
   async beforeMount () {
     this.data = await DoctorService.getAllPharmacists()
     this.original = JSON.parse(JSON.stringify(this.data))
+
+    //Moze da se promeni da se povlaci sa backend-a
+    var allPharmacies = []
+    this.data.forEach(element => allPharmacies = allPharmacies.concat(element.pharmacies));
+    this.options = allPharmacies.filter((item, i, ar) => ar.indexOf(item) === i);
   },
   data () {
     return {
       loading: false,
-      filter: '',
+      filter: {
+        query: null,
+        minMark: null,
+        pharmacy: null
+      },
+      filtersActive: false,
+      options: null,
       rowCount: 10,
       columns: [
         { name: 'name', align: 'center', label: 'Name', field: 'name', sortable: true },
@@ -47,6 +70,44 @@ export default {
   },
 
   methods: {
+    myFilter () {
+      this.filtersActive = false
+      var pharmacyFiltered
+      if(this.filter.pharmacy == null) {
+        pharmacyFiltered = this.data
+      } else {
+        pharmacyFiltered = this.data.filter(row => row.pharmacies.indexOf(this.filter.pharmacy) !== -1)
+        this.filtersActive = true
+      }
+      
+      var markFiltered
+      if(this.filter.minMark == null || this.filter.minMark == '') {
+        markFiltered = pharmacyFiltered
+      } else {
+        markFiltered = pharmacyFiltered.filter(row => row.averageMark >= this.filter.minMark)
+        this.filtersActive = true
+      }
+
+      var nameFiltered
+      if(this.filter.query == null || this.filter.query == '') {
+        nameFiltered = markFiltered
+      } else {
+        nameFiltered = markFiltered.filter(row => (row.name.toLowerCase() + ' ' + row.surname.toLowerCase()).indexOf(this.filter.query.toLowerCase()) !== -1)
+        this.filtersActive = true
+      }
+
+      return nameFiltered
+
+      const lowerTerms = terms ? terms.toLowerCase() : ''
+      return rows.filter(
+        row => cols.some(col => (cellValue(col, row) + '').toLowerCase().indexOf(lowerTerms) !== -1)
+      )
+    },
+    removeFilters() {
+      this.filter.pharmacy = null
+      this.filter.minMark = null
+      this.filter.query = null
+    }
   }
 }
 </script>
