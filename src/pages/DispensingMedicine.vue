b9a6b85d602926cc1f334dc1caab8bce37d48469
stojke13 <stojakovic_djordje@yahoo.com>
2021-01-31 15:09:46 +0100

    feat21 - Page layout update

--- src/pages/DispensingMedicine.vue
+++ src/pages/DispensingMedicine.vue
@@ -1,13 +1,15 @@
 <template>
-<q-page padding>
-    <div style="max-width:500px" padd>
+<q-page-container>
+<q-page padding class="q-gutter-y-xl column items-center ">
+    <div class="text-h4"> Dispensing medicine,enter reservation code: </div>
+    <div style="max-width:500px" class="row">
       <div class="row q-gutter-x-sm">
       <q-input style="width:400px" v-model="id" label="Reservation code:" >
       </q-input>
           <q-btn @click="send"  icon="search"/>
     </div>
     </div>
-    <div v-show="show" >
+    <div v-show="show">
       <p style="" class="text-h4"> Reservation found <q-icon name="done_outline" size="md" color="green"/> </p>
       <p> Patient: {{res.patientName}}  {{res.patientSurname}} </p>
       <p> E-mail: {{res.email}} </p>
@@ -15,6 +17,7 @@
       <q-btn v-bind:disabled="show2" color="primary" @click="handle">Dispend medicine </q-btn> <q-icon v-show="show2" name="done_outline" size="md" color="green"/>
     </div>
 </q-page>
+</q-page-container>
 </template>
 
 <script>
